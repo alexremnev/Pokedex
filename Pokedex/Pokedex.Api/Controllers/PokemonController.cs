60185a29dec508ad9016ebca6ba05a50e4941e46
Alexey Remnev <alexey.remnev@dataart.com>
2021-06-22 13:22:01 +0300

    init project

---
+++ Pokedex/Pokedex.Api/Controllers/PokemonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Pokedex.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        private readonly ILogger<PokemonController> _logger;
+
+        public PokemonController(ILogger<PokemonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public  IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
